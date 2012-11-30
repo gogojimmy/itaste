8d7f01c05d86d71d77e19c7f7ad062058e8e6b5e
Jimmy Kuo <jimmy@gogojimmy.net>
2012-11-30 18:51:25 +0800

    Fix missing link_to

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Itaste::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  #config.action_mailer.default_url_options = { :host => 'http://itaste.tw' }
+  config.action_mailer.default_url_options = { :host => 'http://itaste.tw' }
 end
 
 Itaste::Application.config.middleware.use ExceptionNotifier,
