c63fa89c2ad1dc4c049f400729a90c80c7bce2b8
Jimmy Kuo <jimmy@gogojimmy.net>
2012-11-30 18:58:20 +0800

    Let's try again...

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Itaste::Application.routes.draw do
+  default_url_options host: $EMAIL_CONFIG[:host]
   root :to => "wines#index"
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}, :skip => [:sessions], controllers: {omniauth_callbacks: "omniauth_callbacks"}
   devise_scope :user do
